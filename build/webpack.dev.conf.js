bc3b00abb2d5dd217bf9e6f204315c423bcd1b9b
Will Soto <will.soto9@gmail.com>
2017-07-24 16:20:39 -0400

    chore(webpack.dev): no sourcemaps
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -17,8 +17,7 @@ module.exports = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
-  // cheap-module-eval-source-map is faster for development
-  devtool: '#cheap-module-eval-source-map',
+  devtool: '',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
