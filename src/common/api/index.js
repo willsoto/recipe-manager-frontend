d95fb2b7221e82eecc02affd63c2ca532e829736
Will Soto <will.soto9@gmail.com>
2017-09-09 09:47:22 -0400

    refactor(api): move ApolloClient into API
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- src/common/api/index.js
+++ src/common/api/index.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
 
 export const Api = axios.create({
   baseURL: '/api'
+});
+
+export const apolloClient = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: '/api/graphql',
+    transportBatching: true
+  })
 });
--- src/main.js
+++ src/main.js
@@ -2,20 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import { ApolloClient, createNetworkInterface } from 'apollo-client';
 import VueApollo from 'vue-apollo';
 
 import App from './App';
 import router from './router';
+import { apolloClient } from './common/api';
 
 import 'vuetify/dist/vuetify.min.css';
-
-const apolloClient = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: 'https://recipe-manager.dev/api/graphql',
-    transportBatching: true
-  })
-});
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient
