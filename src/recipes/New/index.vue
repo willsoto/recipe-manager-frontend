df912b6ad0d5e20017a4694ad23b98a8d014d633
Will Soto <will.soto9@gmail.com>
2017-09-10 09:23:08 -0400

    refactor(recipes): redo styles for the new/edit pages
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- src/recipes/New/index.vue
+++ src/recipes/New/index.vue
@@ -1,15 +1,11 @@
 <template>
-  <v-card>
-    <v-card-text>
-      <v-container fluid>
-        <v-layout column class="mb-5">
-          <h4>New Recipe</h4>
-          <v-divider></v-divider>
-        </v-layout>
-        <recipe-form v-bind:recipe="recipe"></recipe-form>
-      </v-container>
-    </v-card-text>
-  </v-card>
+  <v-container fluid>
+    <v-layout column class="mb-5">
+      <h4>New Recipe</h4>
+      <v-divider></v-divider>
+    </v-layout>
+    <recipe-form v-bind:recipe="recipe"></recipe-form>
+  </v-container>
 </template>
 
 <script src="./new.js"></script>
--- src/recipes/common/RecipeForm/index.vue
+++ src/recipes/common/RecipeForm/index.vue
@@ -1,54 +1,85 @@
 <template>
-  <div>
-    <v-layout row>
-      <v-flex xs4>
-        <v-text-field label="Name" v-model="recipe.name"></v-text-field>
+  <v-container grid-list-xl class="pa-0 ma-0">
+    <v-layout row wrap>
+      <v-flex md6>
+        <v-card class="mb-5">
+          <v-card-text>
+            <v-text-field label="Name" v-model="recipe.name"></v-text-field>
+          </v-card-text>
+        </v-card>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Instructions</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-layout column v-for="(instruction, index) in recipe.instructions" :key="index">
+              <v-layout row>
+                <v-flex md3>
+                  <v-text-field label="Time" type="number" v-model="instruction.time"></v-text-field>
+                </v-flex>
+                <v-flex md4>
+                  <v-select label="Duration" item-text="key" item-value="value" v-bind:items="durationValues" v-model="instruction.duration"></v-select>
+                </v-flex>
+              </v-layout>
+              <v-layout row>
+                <v-flex>
+                  <v-text-field label="Text" type="text" multi-line v-model="instruction.text"></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-layout>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat v-on:click="addInstruction">Add Instruction</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+      <v-flex md6>
+        <v-card class="mb-5">
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Ingredients</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <v-layout row v-for="(ingredient, index) in recipe.ingredients" :key="index">
+              <v-flex md3>
+                <v-text-field label="Quantity" type="number" v-model="ingredient.quanity"></v-text-field>
+              </v-flex>
+              <v-flex md3>
+                <v-select label="Unit" v-bind:items="allUnits" v-model="ingredient.unit"></v-select>
+              </v-flex>
+              <v-flex>
+                <v-text-field label="Name" v-model="ingredient.name"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat v-on:click="addIngredient">Add Ingredient</v-btn>
+          </v-card-actions>
+        </v-card>
+        <v-layout row wrap>
+          <v-flex>
+            <v-card>
+              <v-card-title>
+                <div>
+                  <h3 class="headline mb-0">Tags</h3>
+                </div>
+              </v-card-title>
+              <v-card-text>
+                <v-select label="Tags" v-bind:items="allTags" v-model="recipe.tags" multiple chips></v-select>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
       </v-flex>
     </v-layout>
-    <h5 class="mt-5">Ingredients</h5>
-    <v-layout row v-for="(ingredient, index) in recipe.ingredients" :key="index">
-      <v-flex xs1>
-        <v-text-field label="Quantity" type="number" v-model="ingredient.quanity"></v-text-field>
-      </v-flex>
-      <v-flex xs3>
-        <v-select label="Unit" v-bind:items="allUnits" v-model="ingredient.unit"></v-select>
-      </v-flex>
-      <v-flex xs3>
-        <v-text-field label="Name" v-model="ingredient.name"></v-text-field>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-btn primary dark v-on:click="addIngredient">Add Ingredient</v-btn>
-    </v-layout>
-    <h5 class="mt-5">Instructions</h5>
-    <v-layout column v-for="(instruction, index) in recipe.instructions" :key="index">
-      <v-layout row>
-        <v-flex xs1>
-          <v-text-field label="Time" type="number" v-model="instruction.time"></v-text-field>
-        </v-flex>
-        <v-flex xs2>
-          <v-select label="Duration" item-text="key" item-value="value" v-bind:items="durationValues" v-model="instruction.duration"></v-select>
-        </v-flex>
-      </v-layout>
-      <v-layout row>
-        <v-flex xs5>
-          <v-text-field label="Text" type="text" multi-line v-model="instruction.text"></v-text-field>
-        </v-flex>
-      </v-layout>
-    </v-layout>
-    <v-layout row>
-      <v-btn primary dark v-on:click="addIngredient">Add Instruction</v-btn>
-    </v-layout>
-    <h5 class="mt-5">Tags</h5>
-    <v-layout row>
-      <v-flex xs4>
-        <v-select label="Tags" v-bind:items="allTags" v-model="recipe.tags" multiple chips></v-select>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-btn primary dark v-on:click="saveRecipe">Save</v-btn>
-    </v-layout>
-  </div>
+    <v-divider class="mt-4 mb-4"></v-divider>
+    <v-flex>
+      <v-btn class="ma-0" primary dark v-on:click="saveRecipe">Save</v-btn>
+    </v-flex>
+  </v-container>
 </template>
 
 <script src="./form.js"></script>
